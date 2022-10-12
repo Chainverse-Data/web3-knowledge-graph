e5c23ded6e64e4f5db0bea055e2e3662f6315977
Rohan Bansal <robansal@stanford.edu>
2022-10-12 10:23:41 -0700

    fix xqua changes

--- scraping/snapshot/scrape.py
+++ scraping/snapshot/scrape.py
@@ -1,6 +1,6 @@
 from ..helpers import Scraper
 from ..helpers import tqdm_joblib, get_ens_info, str2bool
-from .query_strings import spaces_query, proposals_query, votes_query
+from .helpers.query_strings import spaces_query, proposals_query, votes_query
 import json
 import logging
 import tqdm
@@ -14,34 +14,36 @@ import math
 
 class SnapshotScraper(Scraper):
     def __init__(self, recent):
-        super().__init__("snapshot", allow_override=bool(os.environ["ALLOW_OVERRIDE"]))
+        super().__init__("snapshot")
         self.snapshot_url = "https://hub.snapshot.org/graphql"
         self.recent = recent
         self.spaces_query = spaces_query
         self.proposals_query = proposals_query
         self.votes_query = votes_query
 
-        if "counts" not in self.metadata:
-            self.metadata["counts"] = {}
+        if recent and "last_timestamp" in self.metadata:
+            self.cutoff_timestamp = self.metadata["last_timestamp"] - (60 * 60 * 24 * 15)
+        else:
+            self.cutoff_timestamp = 0
 
     def get_spaces(self):
         raw_spaces = []
         first = int(re.search("first: \d+", self.spaces_query)[0].split(" ")[1])
         offset = 0
-        print("Getting spaces...")
+        logging.info("Getting spaces...")
         content = self.post_request(self.snapshot_url, json={"query": self.spaces_query})
         data = json.loads(content)
         results = data["data"]["spaces"]
         while results:
             raw_spaces.extend(results)
             if len(raw_spaces) % 1000 == 0:
-                print(f"Current Spaces: {len(raw_spaces)}")
+                logging.info(f"Current Spaces: {len(raw_spaces)}")
             offset += int(first)
             new_query = self.spaces_query.replace("skip: 0", f"skip: {offset}")
             content = self.post_request(self.snapshot_url, json={"query": new_query})
             data = json.loads(content)
             results = data["data"]["spaces"]
-        print(f"Total Spaces: {len(raw_spaces)}")
+        logging.info(f"Total Spaces: {len(raw_spaces)}")
 
         with tqdm_joblib(tqdm.tqdm(desc="Getting ENS Data", total=len(raw_spaces))) as progress_bar:
             ens_list = joblib.Parallel(n_jobs=multiprocessing.cpu_count() - 1)(
@@ -53,18 +55,12 @@ class SnapshotScraper(Scraper):
 
         final_spaces = [space for space in raw_spaces if space]
         self.data["spaces"] = final_spaces
-        self.metadata["counts"]["spaces"] = len(final_spaces)
 
     def get_proposals(self):
         raw_proposals = []
         first = int(re.search("first: \d+", self.proposals_query)[0].split(" ")[1])
         offset = 0
-        if self.recent and "last_timestamp" in self.metadata:
-            cutoff_timestamp = self.metadata["last_timestamp"] - (60 * 60 * 24 * 15)
-        else:
-            cutoff_timestamp = 0
-        self.metadata["last_timestamp"] = self.runtime.timestamp()
-        print("Getting proposals...")
+        logging.info("Getting proposals...")
         content = self.post_request(self.snapshot_url, json={"query": self.proposals_query})
         data = json.loads(content)
         results = data["data"]["proposals"]
@@ -73,7 +69,7 @@ class SnapshotScraper(Scraper):
             if len(raw_proposals) % 1000 == 0:
                 print(f"Current Proposals: {len(raw_proposals)}")
             try:
-                if results[-1]["created"] < cutoff_timestamp:
+                if results[-1]["created"] < self.cutoff_timestamp:
                     break
             except:
                 pass
@@ -83,9 +79,8 @@ class SnapshotScraper(Scraper):
             data = json.loads(content)
             results = data["data"]["proposals"]
 
-        print(f"Total Proposals: {len(raw_proposals)}")
+        logging.info(f"Total Proposals: {len(raw_proposals)}")
         self.data["proposals"] = [proposal for proposal in raw_proposals if proposal]
-        self.metadata["counts"]["proposals"] = len(raw_proposals)
 
     def scrape_votes(self, proposal_id_list):
         raw_votes = []
@@ -111,7 +106,7 @@ class SnapshotScraper(Scraper):
 
     def get_votes(self):
         proposal_id_list = [proposal["id"] for proposal in self.data["proposals"]]
-        print("Getting votes...")
+        logging.info("Getting votes...")
         with tqdm_joblib(
             tqdm.tqdm(desc="Getting Votes Data", total=math.ceil(len(proposal_id_list) / 5))
         ) as progress_bar:
@@ -121,14 +116,15 @@ class SnapshotScraper(Scraper):
             )
 
         raw_votes = [vote for votes in raw_votes_list for vote in votes]
-        print(f"Total Votes: {len(raw_votes)}")
+        logging.info(f"Total Votes: {len(raw_votes)}")
         self.data["votes"] = raw_votes
-        self.metadata["counts"]["votes"] = len(raw_votes)
 
     def run(self):
         self.get_spaces()
         self.get_proposals()
         self.get_votes()
+        
+        self.metadata["last_timestamp"] = self.runtime.timestamp()
         self.save_metadata()
         self.save_data()
 
