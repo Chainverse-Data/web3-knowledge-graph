13e8123d8199498a19d1aeb09e9b85b928a5cf8b
Leo Blondel <leo@blondel.ninja>
2022-10-14 11:59:33 +0200

    decorator fix

--- pipelines/helpers/decorators.py
+++ pipelines/helpers/decorators.py
@@ -2,9 +2,9 @@ import logging
 
 def count_query_logging(function):
     "A function wrapped with this decorator must return a count of affected objects."
-    def wrapper():
+    def wrapper(*args, **kwargs):
         logging.info(f"Ingesting with: {function.func_name}")
-        count = function()
+        count = function(*args, **kwargs)
         logging.info(f"Created or merged: {count}")
         return count
     return wrapper
