e045952c76935de629338545e7329c1fe27f765e
Rohan Bansal <robansal@stanford.edu>
2022-10-14 22:05:37 -0700

    remove old ens branch

--- analytics/helpers/analysis.py
+++
@@ -1,9 +0,0 @@
-
-class Analysis():
-    def __init__(self):
-        pass
-
-    def run(self):
-        "Main function to be called. Every postprocessor must implement its own run function!"
-        raise NotImplementedError(
-            "ERROR: the run function has not been implemented!")
