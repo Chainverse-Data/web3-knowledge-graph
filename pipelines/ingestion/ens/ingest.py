34ff19c38bf94277660181d3f938dbb2280316c2
Rohan Bansal <robansal@stanford.edu>
2022-10-22 11:26:55 -0700

    ens ingestion tweaks

--- pipelines/ingestion/ens/ingest.py
+++ pipelines/ingestion/ens/ingest.py
@@ -2,6 +2,7 @@ import logging
 from ..helpers import Ingestor
 from .cyphers import EnsCyphers
 import datetime
+import pandas
 
 
 class EnsIngestor(Ingestor):
@@ -14,12 +15,32 @@ class EnsIngestor(Ingestor):
 
     def ingest_ens(self):
         logging.info("Ingesting ens...")
+
+        df = pandas.DataFrame(self.scraper_data["ens"]).drop_duplicates(subset=["address", "name"])
+        df = df[
+            (df["name"].notna())
+            & (df["address"].notna())
+            & (df["token_id"].notna())
+            & (df["name"] != "")
+            & (df["address"] != "")
+            & (df["token_id"] != "")
+        ]
+        df = df[df["name"].apply(self.utf8len)].rename(columns={"token_id": "tokenId"})
+        self.scraper_data["ens"] = df.to_dict("records")
         urls = self.s3.save_json_as_csv(self.scraper_data["ens"], self.bucket_name, f"ingestor_ens_{self.asOf}")
-        self.cyphers.create_or_merge_ens(urls)
+        self.cyphers.create_or_merge_ens_items(urls)
         self.cyphers.link_ens(urls)
 
+        df = pandas.DataFrame(self.scraper_data["primary"]).drop_duplicates(subset=["address", "name"])
+        df = df[(df["name"].notna()) & (df["address"].notna()) & (df["name"] != "") & (df["address"] != "")]
+        df = df[df["name"].apply(self.utf8len)]
+        self.scraper_data["primary"] = df.to_dict("records")
         urls = self.s3.save_json_as_csv(self.scraper_data["primary"], self.bucket_name, f"ingestor_primary_{self.asOf}")
         self.cyphers.add_primary_property(urls)
+
+    @staticmethod
+    def utf8len(s):
+        return len(s.encode("utf-8")) <= 8164
 
     def run(self):
         self.ingest_ens()
