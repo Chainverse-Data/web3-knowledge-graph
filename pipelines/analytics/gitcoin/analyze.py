3a95ba0b0673da7b9108bc7c99262427d4e48ae5
Leo Blondel <leo@blondel.ninja>
2022-10-26 12:53:46 +0200

    updated

--- pipelines/analytics/gitcoin/analyze.py
+++ pipelines/analytics/gitcoin/analyze.py
@@ -5,6 +5,7 @@ from ..helpers import Analysis, Networks
 from .cyphers import GitCoinAnalyticsCyphers
 
 
+
 class GitCoinAnalysis(Analysis):
     def __init__(self):
         super().__init__("gitcoin-communities")
@@ -17,37 +18,31 @@ class GitCoinAnalysis(Analysis):
         self.create_grant_donor_graph()
         self.grants_partitions()
         self.donors_partitions()
-
+        
     def grants_partitions(self):
-        partitionTarget = "Grant:Gitcoin"
+        partitionTarget = "GitcoinGrant:Grant"
         self.cyphers.clear_partitions(partitionTarget)
         grants_partitions, grants_labels = self.create_grants_communities()
-        data = self.prepare_partitions_data(
-            grants_partitions, grants_labels, partitionTarget)
-
-        urls = self.s3.save_json_as_csv(
-            data["labels"], self.bucket_name, f"grants_partitions_labels_{self.asOf}")
+        data = self.prepare_partitions_data(grants_partitions, grants_labels, partitionTarget)
+        
+        urls = self.s3.save_json_as_csv(data["labels"], self.bucket_name, f"grants_partitions_labels_{self.asOf}")
         self.cyphers.create_or_merge_partitions(urls)
-
-        urls = self.s3.save_json_as_csv(
-            data["partitions"], self.bucket_name, f"grants_partitions_{self.asOf}")
+        
+        urls = self.s3.save_json_as_csv(data["partitions"], self.bucket_name, f"grants_partitions_{self.asOf}")
         self.cyphers.link_partitions(urls, partitionTarget, "id")
-
+        
     def donors_partitions(self):
         partitionTarget = "Wallet"
         self.cyphers.clear_partitions(partitionTarget)
         donors_partitions, donors_labels = self.create_donors_communities()
-        data = self.prepare_partitions_data(
-            donors_partitions, donors_labels, partitionTarget)
-
-        urls = self.s3.save_json_as_csv(
-            data["labels"], self.bucket_name, f"donors_partitions_labels_{self.asOf}")
+        data = self.prepare_partitions_data(donors_partitions, donors_labels, partitionTarget)
+        
+        urls = self.s3.save_json_as_csv(data["labels"], self.bucket_name, f"donors_partitions_labels_{self.asOf}")
         self.cyphers.create_or_merge_partitions(urls)
-
-        urls = self.s3.save_json_as_csv(
-            data["partitions"], self.bucket_name, f"donors_partitions_{self.asOf}")
+        
+        urls = self.s3.save_json_as_csv(data["partitions"], self.bucket_name, f"donors_partitions_{self.asOf}")
         self.cyphers.link_partitions(urls, partitionTarget, "address")
-
+        
     def prepare_partitions_data(self, partitions, labels, partitionTarget):
         logging.info("Preparing grants data...")
         data = {
@@ -72,42 +67,34 @@ class GitCoinAnalysis(Analysis):
                 }
             )
         return data
-
+    
     def create_grants_communities(self):
         logging.info("Creating grants communities...")
-        grants_adj = self.networks.compute_projection(
-            self.biadjacency, self.grants_weight_threshold, axis=0)
-        partitions, labels = self.networks.get_partitions(
-            grants_adj, self.grants_id_map)
+        grants_adj = self.networks.compute_projection(self.biadjacency, self.grants_weight_threshold, axis=0)
+        partitions, labels = self.networks.get_partitions(grants_adj, self.grants_id_map)
         return partitions, labels
 
     def create_donors_communities(self):
         logging.info("Creating donors communities...")
-        donors_adj = self.networks.compute_projection(
-            self.biadjacency, self.donors_weight_threshold, axis=1)
-        partitions, labels = self.networks.get_partitions(
-            donors_adj, self.donors_id_map)
+        donors_adj = self.networks.compute_projection(self.biadjacency, self.donors_weight_threshold, axis=1)
+        partitions, labels = self.networks.get_partitions(donors_adj, self.donors_id_map)
         return partitions, labels
-
+        
     def create_grant_donor_graph(self):
         logging.info("Creating grant - donors graph")
         result = self.cyphers.get_grants_donations_graph()
         self.G = nx.Graph()
 
         for record in tqdm(result):
-            self.G.add_node(record[0], label=record[1]
-                            [-1], tags=record[2], types=record[3])
-            self.G.add_node(record[4], label=record[5][-1])
+            self.G.add_node(record[0], labels=record[1], tags=record[2], types=record[3])
+            self.G.add_node(record[4], labels=record[5])
             self.G.add_edge(record[0], record[4])
+        
+        self.donors = [n for n in self.G.nodes() if len(set(self.G.nodes[n]["labels"]).intersection(set(["Wallet", "MultiSig"]))) > 0 and self.G.degree[n] > 1]
+        self.grants = [n for n in self.G.nodes() if len(set(self.G.nodes[n]["labels"]).intersection(set(["GitcoinGrant", "Grant"]))) > 0 and self.G.degree[n] > 1]
 
-        self.donors = [n for n in self.G.nodes() if self.G.nodes[n]["label"] in [
-            "Wallet", "MultiSig"] and self.G.degree[n] > 1]
-        self.grants = [n for n in self.G.nodes() if self.G.nodes[n]["label"] in [
-            "GitcoinGrant", "Gitcoin"] and self.G.degree[n] > 1]
-
-        self.biadjacency, self.grants_id_map, self.donors_id_map = self.networks.compute_biadjacency(
-            self.G, self.grants, self.donors)
-
+        self.biadjacency, self.grants_id_map, self.donors_id_map = self.networks.compute_biadjacency(self.G, self.grants, self.donors)
+        
 if __name__ == '__main__':
     analytics = GitCoinAnalysis()
     analytics.run()
