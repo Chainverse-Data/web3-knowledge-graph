3cad82265eb5dcdc013d64a2b7db31ca4deccc3b
Jordan Olmstead <chainverse-don@Jordans-MacBook-Pro.local>
2022-12-05 16:03:32 -0800

    some minor changes + ingest of lock metadata

--- pipelines/ingestion/unlock/cyphers.py
+++ pipelines/ingestion/unlock/cyphers.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import logging
 from ...helpers import Cypher
 from ...helpers import Constraints, Indexes, Queries
 from ...helpers import count_query_logging
@@ -11,7 +12,8 @@ class UnlockCyphers(Cypher):
 
         ### ingest wallets
 
-
+    ## should add something to make sure that the number of wallets created / merged matches number of unique
+    ## wallets in the file
     def create_wallets(self, urls):
         count = 0
         for url in urls:
@@ -19,19 +21,41 @@ class UnlockCyphers(Cypher):
             LOAD CSV WITH HEADERS FROM '{url}' as wallets
             MERGE (w:Wallet {{address: wallets.address}})
             ON MATCH SET
+                w:UnlockTestStage,
                 w.lastUpdateDt = datetime(apoc.date.toISO8601(toInteger(wallets.occurDt), 'ms'))
             ON CREATE SET
                 w.uuid = apoc.create.uuid(),
+                w:UnlockTestStage,
                 w.lastUpdateDt = datetime(apoc.date.toISO8601(toInteger(wallets.occurDt), 'ms')),
                 w.createdDt = datetime(apoc.date.toISO8601(toInteger(wallets.occurDt), 'ms'))
             RETURN COUNT(DISTINCT(w))
                 """
             count += self.query(walletsQuery)[0].value()
+            logging.info(f"Nice I created {count} wallets.")
         return count 
     
     def create_locks(self, urls):
-        count = None
-
+        count = 0
+        for url in urls:
+            lockMetadataQuery = f"""
+            LOAD CSV WITH HEADERS FROM '{url}' as locks
+            MERGE (t:Token {{address: locks.address}})  // this needs to change lol
+            ON MATCH SET
+                t:Nft,
+                t:UnlockTestStage,
+                t.lastUpdateDt = datetime(apoc.date.toISO8601(toInteger(locks.occurDt), 'ms')),
+                t.price = locks.price
+            ON CREATE SET
+                t:Nft,
+                t.uuid = apoc.create.uuid(),
+                t:UnlockTestStage,
+                t.lastUpdateDt = datetime(apoc.date.toISO8601(toInteger(locks.occurDt), 'ms')),
+                t.createdDt = datetime(apoc.date.toISO8601(toInteger(locks.occurDt), 'ms'))
+            RETURN
+                COUNT(DISTINCT(t))
+                        """
+            count += self.query(lockMetadataQuery)[0].value()
+            logging.info(f"Nice I created or modified {count} locks.")
         return count
 
             
