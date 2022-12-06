40a3a9185149292bff776d986a4e4c666cd5a6bd
Jordan Olmstead <chainverse-don@Jordans-MacBook-Pro.local>
2022-12-05 21:55:35 -0800

    ugly but works. L2s next

--- pipelines/ingestion/unlock/cyphers.py
+++ pipelines/ingestion/unlock/cyphers.py
@@ -57,6 +57,122 @@ class UnlockCyphers(Cypher):
             count += self.query(lockMetadataQuery)[0].value()
             logging.info(f"Nice I created or modified {count} locks.")
         return count
+    
+    def create_keys(self, urls):
+        count = 0
+        for url in urls:
+            keyMetadataQuery = f"""
+            LOAD CSV WITH HEADERS FROM '{url}' as keys
+            MERGE (t:Token:Nft {{tokenId: keys.keyId, tokenContract: keys.lockAddress}})
+            ON MATCH SET
+                t:UnlockTestStage,
+                t:Instance, // should discuss with Xqua w/r/t what this means for NFT ontology
+                t.lastUpdateDt = datetime(apoc.date.toISO8601(toInteger(keys.occurDt), 'ms')),
+                t.expiration = keys.expiration,
+                t.tokenUri = keys.tokenURI,
+                t.tokenContract = keys.lockAddress
+            ON CREATE SET
+                t:UnlockTestStage,
+                t.uuid = apoc.create.uuid(),
+                t.lastUpdateDt = datetime(apoc.date.toISO8601(toInteger(keys.occurDt), 'ms')),
+                t.createdDt = datetime(apoc.date.toISO8601(toInteger(keys.occurDt), 'ms')),
+                t.tokenUri = keys.tokenURI,
+                t.tokenContract = keys.lockAddress,
+                t.expiration = keys.expiration
+            RETURN
+                COUNT(DISTINCT(t))
+                        """
+            count += self.query(keyMetadataQuery)[0].value()
+            logging.info(f"Nice I created or modified {count} keys.")
+        return count
+
+    def create_lock_managers(self, urls):
+        count = 0 
+        for url in urls:
+            lockManagerQuery = f"""
+            LOAD CSV WITH HEADERS FROM '{url}' as lockManagers
+            MATCH (t:Token:Nft {{address: lockManagers.lockAddress}})
+            MATCH (w:Wallet {{address: lockManagers.lockManager}})
+            WITH 
+                w,
+                t,
+                datetime(apoc.date.toISO8601(toInteger(keys.occurDt), 'ms')) as ts
+            MERGE
+                (w)-[r:HAS_TOKEN]->(t)
+            set 
+                r.lastUpdateDt = ts
+            set
+                r.citation = 'Unlock Subgraph'
+            set
+                r.lastUpdateDt = ts
+            RETURN
+                COUNT(DISTINCT(r))
+                """
+            count += self.query(lockManagerQuery)[0].value()
+            logging.info(f"Nice I created or modified {count} lock managers.")
+        return count
+
+    def create_key_holders(self, urls):
+        count = 0 
+        for url in urls:
+            keyHolderQuery = f"""
+                LOAD CSV WITH HEADERS FROM '{url}' as keyHolders
+                MATCH (t:Token:Nft:Instance {{tokenId: keyHolders.keyId, tokenContract: keyHolders.lockAddress}})
+                MATCH (w:Wallet {{address: keyHolders.owner}})
+                WITH t,w, timestamp() as ts
+                MERGE (w)-[r:HAS_BALANCE]->(t)
+                SET r.lastUpdateDt = ts
+                SET r.createdDt = ts
+                SET r.citation = 'Unlock Subgraph'
+                RETURN COUNT(DISTINCT(w))"""
+            count += self.query(keyHolderQuery)[0].value()
+            logging.info(f"Nice I created or modified {count} key holders.")
+        return count
+
+    def connect_locks_keys(self, urls):
+        count = 0 
+        for url in urls:
+            query = f"""
+            LOAD CSV WITH HEADERS FROM '{url}' as lockKeys
+            WITH collect(distinct(lockKeys.adddress)) as lockAddresses
+            MATCH (t:Token:Nft)
+            MATCH (i:Instance:Token:Nft)
+            WHERE t.address in lockAddresses
+            AND t.address = i.tokenContract
+            AND NOT (t)-[:HAS_INSTANCE]->(i)
+            WITH t, i
+            MERGE (t)-[r:HAS_INSTANCE]->(i)
+            SET r.citation = 'Unlock Subgraph'
+            RETURN count(distinct(t)) as locks, count(distinct(r)) as rels, count(distinct(i)) as keys"""
+            self.query(query)
+            count += 1
+        logging.info(f"Nice I ran {count} queries to connect locks and keys")
+        
+        ## this is fucked up and needs to be fixed
+        checkQuery = f"""
+        LOAD CSV WITH HEADERS FROM '{url}' as lockKeys
+        WITH collect(distinct(lockKeys.adddress)) as lockAddresses
+        MATCH (t:Token:Nft)-[r:HAS_INSTANCE]->(i:Instance:Token:Nft)
+        RETURN COUNT(DISTINCT(t)) as tokens, COUNT(DISTINCT(r)) as rels, COUNT(DISTINCT(i)) as instances
+        """   
+        countQuery = self.query(checkQuery)
+        locks = countQuery[0].value()
+        logging.info(f"Nice I connected  {locks} locks to their keys.")
+        return count
+
+            
+            
+
+
+
+
+                
+                
+
+            
+            
+            
+
 
             
 
--- pipelines/ingestion/unlock/ingest.py
+++ pipelines/ingestion/unlock/ingest.py
@@ -20,7 +20,6 @@ class UnlockIngestor(Ingestor):
             lockRows = len(locksDf),
             keyRows = len(keysDf)
         ))
-
         ### object metadata
         lockMetadata = locksDf.drop(columns=['keys', 'lockManagers'])
         keyMetadata = keysDf.filter(['lockAddress', 'keyId', 'expiration', 'tokenURI'])
@@ -28,7 +27,7 @@ class UnlockIngestor(Ingestor):
         ### relationships
         lockManagers = locksDf.filter(['address', 'lockManagers'])
         lockManagers = lockManagers.explode('lockManagers')
-        lockManagers = lockManagers.rename(columns={'lockManagers':'lockManager'})
+        lockManagers = lockManagers.rename(columns={'lockManagers':'lockManager', 'address': 'lockAddress'})
         keyHolders = keysDf.filter(['owner', 'lockAddress', 'keyId'])
         keyHolders['tokenId'] = keyHolders['keyId'].apply(lambda x: x.split('-')[1])
 
@@ -39,34 +38,71 @@ class UnlockIngestor(Ingestor):
         allWalletsUnique = list(set(allWallets))
         allWalletsUnique = pd.DataFrame()
         allWalletsUnique['address'] = allWallets
+        
+
+        ## tokens
+        tokensUnique = list(set(locksDf.filter(['lockAddress'])))
+        allTokensUnique = pd.DataFrame(data=tokensUnique, columns=['address'])
+
 
         return {
             'lockMetadata':lockMetadata, 'keyMetadata':keyMetadata, 'lockManagers':lockManagers, 'keyHolders': keyHolders,
-            'allWalletsUnique': allWalletsUnique
-        }
+            'allWalletsUnique': allWalletsUnique, 'allTokensUnique': allTokensUnique
+        } 
 
-    def ingest_wallets(self): 
+    def ingest_wallets(self, data=None): 
         logging.info("Ingesting wallets...")
-        
-        unlockData = self.prepare_unlock_data()
-        allWalletsUnique = unlockData['allWalletsUnique'] 
+        allWalletsUnique = data['allWalletsUnique'] 
         urls = self.s3.save_df_as_csv(allWalletsUnique, self.bucket_name, f"ingestor_wallets_{self.asOf}", ACL='public-read')
         self.cyphers.create_wallets(urls)
         logging.info(f"successfully ingested wallets. good job dev!")
     
-    def ingest_lock_metadata(self):
+    def ingest_lock_metadata(self, data=None):
         logging.info("Ingesting lock metadata...")
-
-        unlockData = self.prepare_unlock_data()
-        lockMetadata = unlockData['lockMetadata']
+        lockMetadata = data['lockMetadata']
         urls = self.s3.save_df_as_csv(lockMetadata, self.bucket_name, f"ingestor_locks_{self.asOf}", ACL='public-read') 
         self.cyphers.create_locks(urls)
         logging.info("successfully ingested locks. good job dev!")
+    
+    def ingest_key_metadata(self, data=None):
+        logging.info("Ingesting key metadata...")
+        keyMetadata = data['keyMetadata']
+        urls = self.s3.save_df_as_csv(keyMetadata, self.bucket_name, f"ingestor_keys_{self.asOf}", ACL='public-read')
+        self.cyphers.create_keys(urls)
+        logging.info("successfully ingested keys. good job dev")
+    
+    def ingest_lock_managers(self, data=None):
+        logging.info("Ingesting lock managers...")
+        lockManagers = data['lockManagers']
+        urls = self.s3.save_df_as_csv(lockManagers, self.bucket_name, f"ingestor_lock_managers_{self.asOf}", ACL='public-read')
+        self.cyphers.create_lock_managers(urls)
+        logging.info("successfully ingested lock managers. good job dev!")
+
+    def ingest_key_holders(self, data=None):
+        logging.info("Ingesting key holders...")
+        keyHolders = data['keyHolders']
+        urls = self.s3.save_df_as_csv(keyHolders, self.bucket_name, f"ingestor_key_holders_{self.asOf}", ACL='public-read')
+        self.cyphers.create_key_holders(urls)
+        logging.info("successfully ingested key holders. good job dev!")
+    
+    def ingest_locks_keys(self, data=None):
+        logging.info("Connecting locks and keys...")
+        locks = data['allTokensUnique']
+        urls = self.s3.save_df_as_csv(locks, self.bucket_name, f"ingest_locks_keys{self.asOf}", ACL='public-read')
+        self.cyphers.connect_locks_keys(urls)
+        logging.info("successfully connected locks to keys. good job dev!")
+
 
     def run(self):
-        self.prepare_unlock_data()
-        self.ingest_wallets()
-        self.ingest_lock_metadata()
+        unlockData = self.prepare_unlock_data()
+        #self.ingest_wallets(data=unlockData)
+       # self.ingest_lock_metadata(data=unlockData)
+       # self.ingest_key_metadata(data=unlockData)
+       # self.ingest_lock_managers(data=unlockData)
+      #  self.ingest_key_holders(data=unlockData)
+        self.ingest_locks_keys(data=unlockData)
+
+
 
 if __name__ == "__main__":
     ingestor = UnlockIngestor()
