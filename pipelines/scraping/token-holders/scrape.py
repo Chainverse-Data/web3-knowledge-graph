4507bce9d321e7482427964c69babb4d1993539e
Leo Blondel <xqua@free.fr>
2022-12-09 12:17:07 +0100

    tokenHolder refactor

--- pipelines/scraping/token-holders/scrape.py
+++ pipelines/scraping/token-holders/scrape.py
@@ -1,10 +1,14 @@
+import multiprocessing
 import time
 import json
 import os
+import joblib
 from tqdm import tqdm
 from ..helpers import Scraper
 from .cyphers import TokenHoldersCypher
 import logging
+from ..helpers import tqdm_joblib
+
 DEBUG = os.environ.get("DEBUG", False)
 # I have to add try except statements to this because the overall api calls from alchemy have a 1/20000 chance of SSL failure.
 # This is an issue as there are over a million wallets as of dec 2022
@@ -17,6 +21,9 @@ class TokenHolderScraper(Scraper):
         self.wallets_last_block = self.metadata.get("wallets_last_block", {})
         self.alchemy_api_url = "https://eth-mainnet.g.alchemy.com/v2/{}".format(os.environ["ALCHEMY_API_KEY"])
         self.get_current_block()
+        self.max_thread = 100
+        if DEBUG:
+            self.max_thread = 10
 
     def get_current_block(self):
         headers = {"Content-Type": "application/json"}
@@ -31,35 +38,92 @@ class TokenHolderScraper(Scraper):
 
     def get_all_wallets_in_db(self):
         self.wallet_list  = self.cyphers.get_all_wallets()
+        if DEBUG:
+            self.wallet_list = self.wallet_list[:1000]
+
+    def job_get_transactions(self, wallet):
+        assets = set()
+        transactions = {}
+        tokens = {}
+        transactions["received"] = self.get_received_transactions(wallet, self.wallets_last_block.get(wallet, 0))
+        transactions["sent"] = self.get_sent_transactions(wallet, self.wallets_last_block.get(wallet, 0))
+        for transaction in transactions["received"] + transactions["sent"]:
+            if transaction["category"] in ["erc20", "erc721", "erc1155"]:
+                contractAddress = transaction["rawContract"]["address"]
+                if contractAddress not in tokens:
+                    tokens[contractAddress] = {
+                        "contractType": transaction["category"],
+                        "symbol": transaction["asset"],
+                        "decimal": transaction["rawContract"]["decimal"],
+                    }
+                assets.add(contractAddress)
+        assets = list(assets)
+        return (wallet, assets, transactions, tokens)
+
+    def job_get_balances(self, wallet):
+        balances = self.get_balances(wallet, self.data["assets"][wallet])
+        return (wallet, balances)
+
+#  def get_transactions_assets_balances(self):
+#         logging.info("Getting all transactions assets and balances")
+#         transactions = {}
+#         balances = {}
+#         assets = {}
+#         tokens = {}
+#         with tqdm_joblib(tqdm.tqdm(desc="Getting ENS Data", total=len(self.data["owner_addresses"]))) as progress_bar:
+#             ens_list = joblib.Parallel(n_jobs=multiprocessing.cpu_count() - 1, backend="threading")
+#             (
+#                 joblib.delayed(self.get_ens_info)(address) for address in self.data["owner_addresses"]
+#             )
+#         for wallet in tqdm(self.wallet_list):
+#             assets[wallet] = set()
+#             transactions[wallet] = {}
+#             transactions[wallet]["received"] = self.get_received_transactions(wallet, self.wallets_last_block.get(wallet, 0))
+#             transactions[wallet]["sent"] = self.get_sent_transactions(wallet, self.wallets_last_block.get(wallet, 0))
+#             for transaction in transactions[wallet]["received"] + transactions[wallet]["sent"]:
+#                 if transaction["category"] in ["erc20", "erc721", "erc1155"]:
+#                     contractAddress = transaction["rawContract"]["address"]
+#                     if contractAddress not in tokens:
+#                         tokens[contractAddress] = {
+#                             "contractType": transaction["category"],
+#                             "symbol": transaction["asset"],
+#                             "decimal": transaction["rawContract"]["decimal"],
+#                         }
+#                     assets[wallet].add(contractAddress)
+#             assets[wallet] = list(assets[wallet])
+
+#             balances[wallet] = self.get_balances(wallet, assets[wallet])
+#             self.wallets_last_block[wallet] = self.current_block
+#         self.data["transactions"] = transactions
+#         self.data["balances"] = balances
+#         self.data["assets"] = assets
+#         self.data["tokens"] = tokens
 
     def get_transactions_assets_balances(self):
         logging.info("Getting all transactions assets and balances")
-        transactions = {}
-        balances = {}
-        assets = {}
-        tokens = {}
+        self.data["transactions"] = {}
+        self.data["balances"] = {}
+        self.data["assets"] = {}
+        self.data["tokens"] = {}
+        logging.info("Multithreaded scraping launching!")
+        with tqdm_joblib(tqdm.tqdm(desc="Getting transactions data", total=len(self.wallet_list))) as progress_bar:
+            data = joblib.Parallel(n_jobs=multiprocessing.cpu_count() - 1, backend="threading")(joblib.delayed(self.job_get_transactions)(wallet) for wallet in self.wallet_list)
+        for item in tqdm(data):
+            wallet, assets, transactions, tokens = item
+            self.data["assets"][wallet] = assets
+            self.data["transactions"][wallet] = transactions
+            for token in tokens:
+                if token not in self.data["tokens"]:
+                    self.data["tokens"][token] = tokens[token]
+        
+        with tqdm_joblib(tqdm.tqdm(desc="Getting balances data", total=len(self.wallet_list))) as progress_bar:
+            data = joblib.Parallel(n_jobs=multiprocessing.cpu_count() - 1, backend="threading")(joblib.delayed(self.job_get_balances)(wallet) for wallet in self.wallet_list)
+        for item in tqdm(data):
+            wallet, balances = item
+            self.data["balances"][wallet] = balances
+
         for wallet in tqdm(self.wallet_list):
-            assets[wallet] = set()
-            transactions[wallet] = {}
-            transactions[wallet]["received"] = self.get_received_transactions(wallet, self.wallets_last_block.get(wallet, 0))
-            transactions[wallet]["sent"] = self.get_sent_transactions(wallet, self.wallets_last_block.get(wallet, 0))
-            for transaction in transactions[wallet]["received"] + transactions[wallet]["sent"]:
-                if transaction["category"] in ["erc20", "erc721", "erc1155"]:
-                    contractAddress = transaction["rawContract"]["address"]
-                    if contractAddress not in tokens:
-                        tokens[contractAddress] = {
-                            "contractType": transaction["category"],
-                            "symbol": transaction["asset"],
-                            "decimal": transaction["rawContract"]["decimal"],
-                        }
-                    assets[wallet].add(contractAddress)
-            assets[wallet] = list(assets[wallet])
-            balances[wallet] = self.get_balances(wallet, assets[wallet])
             self.wallets_last_block[wallet] = self.current_block
-        self.data["transactions"] = transactions
-        self.data["balances"] = balances
-        self.data["assets"] = assets
-        self.data["tokens"] = tokens
 
     def alchemy_API_call_iterate(self, payload, key, pagekey=1, counter=0, results=[]):
         if counter > 10:
