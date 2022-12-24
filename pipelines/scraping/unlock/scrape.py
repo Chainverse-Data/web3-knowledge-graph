7a850fab5de7a5b9050eb6e7d71e1953926c7c4a
Jordan Olmstead <chainverse-don@Jordans-MacBook-Pro.local>
2022-12-24 13:05:18 -0800

    unlock rewrite

--- pipelines/scraping/unlock/scrape.py
+++ pipelines/scraping/unlock/scrape.py
@@ -4,6 +4,7 @@ from ..helpers import str2bool
 import os
 import argparse
 import gql 
+import pandas as pd
 import logging 
 from dotenv import load_dotenv
 from gql.transport.aiohttp import AIOHTTPTransport, log as gql_log
@@ -15,31 +16,43 @@ load_dotenv()
 graph_api_key = os.getenv('GRAPH_API_KEY')
 
 class LockScraper(Scraper):
-    def __init__(self, bucket_name='unlock-test', allow_override=True):
+    def __init__(self, bucket_name='unlock-demo', allow_override=True):
         super().__init__(bucket_name, allow_override=allow_override)
 
         ## graph urls
-        self.mainnet_url = f"https://gateway.thegraph.com/api/{graph_api_key}/subgraphs/id/8u7KcVRxjtTDRgEJup3UuPJk6YoRDTHNpSMk5BEpdw42" ## what is this pattern
-        self.mainnet_url_updated = f"https://gateway.thegraph.com/api/{graph_api_key}/subgraphs/id/QmRiwYpBoPESYDf3UhneJc1veUWp1JHUc1rVuT8RWdndCW" ## what is this pattern
-        self.polygon_url = f"https://gateway.thegraph.com/api/{graph_api_key}/subgraphs/id/QmSdGt5e3nymtWY6EhMxfkHCjF9Asr4vvjmtaiKuy6qwAi"
-        self.xdai_url = f"https://gateway.thegraph.com/{graph_api_key}/subgraphs/id/QmUa26Qj9MZn2fmD67SzegPiHSHvFfgYytjFAmxK7au6MY"
-        self.optimism_url = f"https://gateway.thegraph.com/{graph_api_key}/subgraphs/id/QmYWbG6fFM9uaq5zYLwJABSnycGU6s9D9FuqHLDREXhCWP"
+        self.graph_urls = [
+            {
+                "network": "mainnet",
+                "url": "https://api.thegraph.com/subgraphs/name/unlock-protocol/unlock"
+            },
+            {
+                "network": "mainnet", 
+                "url": "https://thegraph.com/hosted-service/subgraph/unlock-protocol/polygon-v2"
+            },
+            {
+                "network": "xdai",
+                "url": "https://thegraph.com/hosted-service/subgraph/unlock-protocol/xdai"
+            },
+            {
+                "network": "optimism",
+                "url": "https://api.thegraph.com/subgraphs/name/unlock-protocol/optimism"
+            },
+            {
+                "network": "arbitrum",
+                "url": "https://api.thegraph.com/subgraphs/name/unlock-protocol/arbitrum-v2"
+            }
+        ]
 
         self.metadata['cutoff_block'] = self.metadata.get("cutoff_block", 9227851) ## doublecheck this
         self.interval = 1000 ## default for The Graph
         self.data['locks'] = ['init'] ## lock data
+        self.data['managers'] = ['init']
         self.data['keys'] = ['init'] ## key data
-        self.mainnet_url
-        
-        ## strategy
-        ### the non mainnet locks have a slightly different ontology. 
-        ### so i can extend `call_the_graph_api` to take a url as an argument and then just call it with the appropriate url, and then rewrite ingestors
-        ### orrr i can extend `call_the_graph_api` to get the new data, and rewrite other functions for the other networks. 
-        ### probably should rewrite everything. but i guess this will work in the meantime
+        self.data['holders'] = ['init']
     
-    def call_the_graph_api(self, query, variables, counter=0):
+    def call_the_graph_api(self, graph_url, network, query, variables, counter=0):
         time.sleep(counter)
-        transport = AIOHTTPTransport(url = self.mainnet_url)
+        transport = AIOHTTPTransport(url = graph_url)
         client = gql.Client(transport=transport, fetch_schema_from_transport=True)
         
         try: 
@@ -60,78 +73,117 @@ class LockScraper(Scraper):
         return(result)
 
     def get_locks(self):
-        skip = 0 
-        cutoff_block = self.metadata['cutoff_block']
-        retry = 0
-        req = 0 
-        max_req = 5
-        locks_before = len(self.data['locks'])
+        for url in self.graph_urls:
+            graph_url = url['url']
+            logging.info(graph_url)
+            network = url['network']
+            skip = 0 
+            cutoff_block = self.metadata['cutoff_block']
+            retry = 0
+            req = 0 
+            max_req = 5
+            locks_before = len(self.data['locks'])
         
-        while len(self.data['locks']) > 0:
-            
-            variables = {
-                "first": self.interval, 
-                "skip": skip, 
-                "cutoff": cutoff_block
-            }
-            query = gql.gql(
-            """query($first: Int!, $skip: Int!, $cutoff: BigInt!) {
-            locks(first: $first, skip: $skip, orderBy: createdAtBlock, orderDirection: desc, where: {createdAtBlock_gt: $cutoff}) {
-                address
-                name
-                tokenAddress
-                createdAtBlock
-                price
-                expirationDuration
-                lockManagers
-                keys {
-                    id
-                    owner
-                    expiration
-                    tokenURI
+            while len(self.data['locks']) > 0:
+                
+                variables = {
+                    "first": self.interval, 
+                    "skip": skip, 
+                    "cutoff": cutoff_block
                 }
-                version
-            }
-            }
-            """)
-            result = self.call_the_graph_api(query, variables)
-            time.sleep(2) ## need to add actual logic here for when to sleep 
-            locks = result['locks']
-            if len(locks) > 0:
-                for lock in locks:
-                    tmp = {
-                        'address': lock['address'].lower(),
-                        'name': lock['name'],
-                        'blockNumber': lock['createdAtBlock'],
-                        'price': lock['price'],
-                        'expirationDuration': lock['expirationDuration'],
-                        'lockManagers': lock['lockManagers'],
-                        'keys': lock['keys']
-                    }
-                    self.data['locks'].append(tmp)
-                    print(tmp)
-                for lock in locks:
-                    address = lock['address'].lower()
-                    for key in lock['keys']:
-                        tmp = {
-                            'lockAddress': address,
-                            'keyId': key['id'],
-                            'owner': key['owner'],
-                            'expiration': key['expiration'],
-                            'tokenURI': key['tokenURI']
+
+                locks_query = gql.gql(
+                    """
+                    query($first: Int!, $skip: Int!, $cutoff: BigInt!) {
+                    locks(first: $first, skip: $skip, orderBy: creationBlock, orderDirection: desc, where: {creationBlock_gt: $cutoff}) {
+                            id
+                            address
+                            name
+                            tokenAddress
+                            creationBlock
+                            price
+                            expirationDuration
+                            totalSupply
+                            LockManagers {
+                                id
+                                address
+                            }
+                            keys {
+                                id
+                                keyId
+                                owner { 
+                                    id
+                                    address
+                                }
+                                expiration
+                                tokenURI
+                                createdAt
+                            }
                         }
-                        self.data['keys'].append(tmp)
-                skip += self.interval
-                logging.info(f"Query success, skip is at: {skip}")                    
-            else: ## changing
-                retry += 1
-                if retry > 5:
-                    skip += self.interval
-                    break ## trying to break if we don't get results
+                        }
+                        """)
 
-    ## def get_key_transactions(self): 
-        ## skip = 0 ###
+                result = self.call_the_graph_api(query=locks_query, variables=variables, graph_url=graph_url, network=network)
+                time.sleep(2)
+                locks = result['locks']
+                for l in locks:
+                    locks_tmp = {
+                        "address": l['address'].lower(),
+                        "id": l['id'],
+                        "name": l['name'],
+                        "tokenAddress": l['tokenAddress'],
+                        "creationBlock": l["creationBlock"],
+                        "price": l["price"],
+                        "expirationDuration": l["expirationDuration"],
+                        "totalSupply": l["totalSupply"],
+                        "network": network
+                    }
+                    self.data['locks'].append(locks_tmp)
+                    df = pd.DataFrame(self.data['locks'])
+                    df.to_csv('locksss.csv')
+                for lc in locks:
+                    for manager in lc['LockManagers']:
+                        managers_tmp = {
+                            "id": manager['id'],
+                            "lock": lc['tokenAddress'],
+                            "address": manager['address']
+                        }
+                        self.data['managers'].append(managers_tmp)
+                        df = pd.DataFrame(self.data['managers'])
+                        df.to_csv('managerss.csv')
+                for l in locks:
+                    address = l['tokenAddress']
+                    for k in l['keys']:
+                        keyId = k['keyId']
+                        keys_tmp = {
+                            "id": k['id'],
+                            "address": address,
+                            "keyId": k['keyId'],
+                            "expiration": k['expiration'],
+                            "tokenURI": k['tokenURI'],
+                            "createdAt": k['createdAt'],
+                            "network": network
+                        }
+                        self.data['keys'].append(keys_tmp)
+                        df = pd.DataFrame(self.data['keys'])
+                        df.to_csv('keysss.csv') 
+                        
+                        holders_tmp = {
+                            "id": k['owner']['id'],
+                            "address": k['owner']['address'],
+                            "keyId":  keyId, 
+                            "address": address
+                        }
 
+                        self.data['holders'].append(holders_tmp)
+                        df = pd.DataFrame(self.data['holders'])
+                        df.to_csv("holdeerss.csv")
+                if skip < 5000:
+                    skip += self.interval 
+                    logging.info(f"Query success, skip is at: {skip}")
+                else: 
+                    logging.info("Next URL")
+                    break                
             
     def run(self):
         self.get_locks()
