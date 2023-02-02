912c8340ed33226b40c4c343c47c04df9d5a25be
Rohan Bansal <robansal@stanford.edu>
2023-02-01 16:24:52 -0800

    farcaster user pipeline

---
+++ pipelines/ingestion/farcaster/cyphers.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import logging
+from ...helpers import Cypher
+from ...helpers import Constraints, Indexes, Queries
+from ...helpers import count_query_logging
+
+
+class FarcasterCyphers(Cypher):
+    def __init__(self):
+        super().__init__()
+        self.queries = Queries()
+
+    def create_farcaster_wallets(self, urls):
+        count = self.queries.create_wallets(urls)
+        return count
+
+    @count_query_logging
+    def create_users(self, urls):
+        count = 0
+        for url in urls:
+            profile_query = f"""
+                                LOAD CSV WITH HEADERS FROM '{url}' as users
+                                MERGE (a:Account:Farcaster {{id: users.id}})
+                                ON MATCH SET
+                                    a.name = users.fname,
+                                    a.address = users.address,
+                                    a.url = users.url,
+                                    a.lastUpdateDt = datetime(apoc.date.toISO8601(apoc.date.currentTimestamp(), 'ms'))
+                                ON CREATE SET
+                                    a.name = users.fname,
+                                    a.address = users.address,
+                                    a.url = users.url,
+                                    a.fId = toInteger(users.fId),
+                                    a.uuid = apoc.create.uuid(),
+                                    a.lastUpdateDt = datetime(apoc.date.toISO8601(apoc.date.currentTimestamp(), 'ms')),
+                                    a.createdDt = datetime(apoc.date.toISO8601(apoc.date.currentTimestamp(), 'ms'))
+                                RETURN
+                                    COUNT(DISTINCT(a))
+                        """
+            count += self.query(profile_query)[0].value()
+        return count
+
+    @count_query_logging
+    def link_users_wallets(self, urls):
+        count = 0
+        for url in urls:
+            profileQuery = f"""
+                            LOAD CSV WITH HEADERS FROM '{url}' as users
+                            MATCH (a:Account:Farcaster {{id: users.id}}), (w:Wallet {{address: users.address}})
+                            MERGE (w)-[r:HAS_ACCOUNT]->(a)
+                            RETURN COUNT(r)
+                """
+            count += self.query(profileQuery)[0].value()
+
+        return count
+    
