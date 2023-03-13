969d6446f6d959c6e59ef79ad93a766733a4a86d
Leo <leo@jogl.io>
2023-03-13 11:40:02 +0100

    added tokens

--- pipelines/post-processing/contract-deployers/cyphers.py
+++ pipelines/post-processing/contract-deployers/cyphers.py
@@ -5,13 +5,18 @@ from ...helpers import get_query_logging, count_query_logging
 from ...helpers import Cypher
 import os
 
-DEBUG = os.environ.get("DEBUG", False)
+# DEBUG = os.environ.get("DEBUG", False)
+DEBUG = False
 
 class ContractDeployersCyphers(Cypher):
     def __init__(self, database=None):
         self.queries = Queries()
         super().__init__(database)
-    
+
+    def create_indexes(self):
+        query = "CREATE INDEX DeployedTx IF NOT EXISTS FOR ()-[r:DEPLOYED]->() ON (r.txHash)"
+        self.query(query)
+
     @get_query_logging
     def get_multisigs(self):
         query = f"""
@@ -46,9 +51,8 @@ class ContractDeployersCyphers(Cypher):
                 LOAD CSV WITH HEADERS FROM '{url}' AS deployers
                 MATCH (wallet:Wallet {{address: toLower(deployers.address)}})
                 MATCH (contract:{label} {{address: toLower(deployers.contractAddress)}})
-                MERGE (wallet)-[link:DEPLOYED]->(contract)
-                SET link.txHash = deployers.txHash,
-                    link.citation = "Etherescan API"
+                MERGE (wallet)-[link:DEPLOYED {{txHash: deployers.txHash}}]->(contract)
+                SET link.citation = "Etherescan API"
                 RETURN count(link)
             """
             count += self.query(query)[0].value()
