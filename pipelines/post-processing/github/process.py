73ba891e7b31d83fe47be46f1ba52e8f9cb16819
Leo Blondel <xqua@free.fr>
2023-03-14 18:42:42 +0100

    smaller chunk size too

--- pipelines/post-processing/github/process.py
+++ pipelines/post-processing/github/process.py
@@ -15,7 +15,7 @@ class GithubProcessor(Processor):
         self.contributor_keys = ["login", "contributions"]
         self.repository_keys = ["id","name","full_name","private","owner","html_url","description","fork","created_at","updated_at","pushed_at","homepage","size","stargazers_count","watchers_count","language","has_issues","has_projects","has_downloads","has_wiki","has_pages","has_discussions","forks_count","mirror_url","archived","disabled","open_issues_count","license","allow_forking","is_template","web_commit_signoff_required","topics","visibility","forks","open_issues","watchers"]
         
-        self.chunk_size = 100
+        self.chunk_size = 10
         super().__init__("github-processing")
 
     def init_data(self):
