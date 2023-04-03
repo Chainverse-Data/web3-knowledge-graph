0111e8c243b1bd152f02019e48fec422984d4ae5
Leo Blondel <xqua@free.fr>
2023-04-03 10:50:50 +0200

    better twitter

--- pipelines/helpers/twitter.py
+++ pipelines/helpers/twitter.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timezone
+import logging
 import os
+import time
 
 import numpy as np
 from .requests import Requests
@@ -11,13 +14,38 @@ class Twitter(Requests):
         self.api_url = "https://api.twitter.com/2"
         self.tweets_api_url = self.api_url + "/tweets"
         self.twitter_api_tokens = [el.strip() for el in os.environ.get("TWITTER_BEARER_TOKEN", "").split(",")]
+        self.rate_limited = {}
+        for token in self.twitter_api_tokens:
+            self.rate_limited[token] = False
+
+    def check_rate_limit(self, response, token):
+        try:
+            content = response.json()
+        except:
+            return False
+        if content["title"] == "Too Many Requests":
+            self.rate_limited[token] = True
+            time_to_wait = int(response.headers["x-rate-limit-reset"]) - int(datetime.now(timezone.utc).timestamp())
+            logging.warning(f"Rate limit exceeded. Waiting {time_to_wait} seconds.")
+            time.sleep(time_to_wait)
+            self.rate_limited[token] = False
 
     def get_headers(self):
         token = np.random.choice(self.twitter_api_tokens)
+        while self.rate_limited[token]:
+            token = np.random.choice(self.twitter_api_tokens)
         twitter_headers = {
             "Authorization": f"Bearer {token}",
         }
         return twitter_headers
+
+    def get_request(self, url, params=None, counter=0):
+        if counter > 10:
+            return None
+        response = super().get_request(url, params=params, headers=self.get_headers(), json=False, decode=False, ignore_retries=True)
+        if not self.check_rate_limit(response) or response.status_code != 200:
+            return self.get_request(url, params=params, counter=counter+1)
+        return response.json()
 
     def search_tweet(self, query, user_info=False, since_id=None, tweets=[], users=[], meta={"newest_id": 0, "oldest_id": np.inf}, max_results=100, next_token=None):
         if DEBUG and len(tweets) > 500:
@@ -32,7 +60,7 @@ class Twitter(Requests):
         if since_id: params["since_id"] = since_id
         if next_token: params["next_token"] = next_token
         url = self.tweets_api_url + "/search/recent"
-        result = self.get_request(url, params=params, headers=self.get_headers(), json=True)
+        result = self.get_request(url, params=params)
         if result and type(result) == dict:
             if "data" in result: 
                 tweets.extend(result["data"])
