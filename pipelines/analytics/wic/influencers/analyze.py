c4cddbd365292de3c06b367f2fb49b6e552b7921
jordan <jordan@chainversedata.com>
2023-04-10 18:36:58 +0100

    influencers + creators

--- pipelines/analytics/wic/influencers/analyze.py
+++ pipelines/analytics/wic/influencers/analyze.py
@@ -3,32 +3,61 @@ from ..WICAnalysis import TYPES
 from .cyphers import InfluencersCyphers
 import logging
  
-class ProfessionalsAnalysis(WICAnalysis):
+class InfluencersAnalysis(WICAnalysis):
     """Identitifes people who "work" in Web3"""
     def __init__(self):
         self.conditions = {
-            "SocialMedia" : {
+            "ContentCreation" : {
+                "MirrorInfluencer": {
+                    "types": [TYPES["influence"]],
+                    "definition": "TBD",
+                    "call": self.process_mirror_influencer
+                },
+                "SubstackInfluencer": {
+                    "types": [TYPES["influence"]],
+                    "definition": "TBD",
+                    "call": self.process_substack_influencer
+                },
+                "DuneWizard": {
+                    "types": [TYPES["influence"]],
+                    "definition": "TBD",
+                    "call": self.process_dune_wizard
+                },
+                "InfluentialPodcaster": {
+                    "types": [TYPES['influence']],
+                    "definition": "TBD",
+                    "call": self.process_podcasters
+                }
+            },
+            "SocialMedia": {
                 "TwitterInfluencer": {
                         "types": [TYPES["influence"]],
                         "definition": "TBD",
-                        "call": self.process_podcaster_bios
-                    },
-                "Podcaster": {
-                        "types": [TYPES["influence"]],
-                        "definition": "TBD",
                         "call": self.process_twitter_influencers
-                    },
+                    }
             }
         }
         self.subgraph_name = "Influencers"
         self.cyphers = InfluencersCyphers(self.subgraph_name, self.conditions)
         super().__init__("wic-influencers")
+
+
+    def process_mirror_influencer(self, context):
+        logging.info("getting mirror influencers...")
+        self.cyphers.get_mirror_influencer(context)
+    
+    def process_substack_influencer(self, context):
+        logging.info("getting substack influencers...")
+        self.cyphers.get_substack_influencer(context)
  
-    def process_podcaster_bios(self, context):
+    def process_podcasters(self, context):
         logging.info("Identifying podcasters based on bios...")
-        queryString = "'podcasts' OR 'podcast'"
-        self.cyphers.identify_podcasters_bios(context, queryString)
- 
+        self.cyphers.identify_podcasters(context)
+
+    def process_dune_wizard(self, context):
+        logging.info("Process dune wizards...")
+        self.cyphers.identify_dune_influencers(context)
+
     def process_twitter_influencers(self, context):
         logging.info("identifying influencers omg..")
         self.cyphers.identify_twitter_influencers(context)
@@ -37,5 +66,5 @@ class ProfessionalsAnalysis(WICAnalysis):
         self.process_conditions()
  
 if __name__ == '__main__':
-    analysis = ProfessionalsAnalysis()
+    analysis = InfluencersAnalysis()
     analysis.run()
--- pipelines/analytics/wic/influencers/cyphers.py
+++ pipelines/analytics/wic/influencers/cyphers.py
@@ -4,10 +4,48 @@ from ....helpers import count_query_logging
 class InfluencersCyphers(WICCypher):
     def __init__(self, subgraph_name, conditions, database=None):
         WICCypher.__init__(self, subgraph_name, conditions, database)
- 
+
+
+    @count_query_logging
+    def get_mirror_influencer(self, context):
+        mirrorThreshold = """
+        MATCH (collector:Wallet)-[hold:HOLDS_TOKEN]->(:Token)-[:HAS_NFT]-(mirror:Mirror)
+        MATCH (author:Wallet)-[aut:AUTHOR]->(mirror)
+        WITH author, COUNT(DISTINCT(collector)) as collectors
+        RETURN apoc.agg.percentiles(collectors)[3] * .9 as threshold"""
+        threshold = self.query(mirrorThreshold)[0].value()
+        
+        mirrorConnect = f"""
+        MATCH (author:Wallet)-[aut:AUTHOR]->(mirror)-[:HAS_NFT]-(:Token)-[:HOLDS_TOKEN]-(collector:Wallet)
+        WITH author, count(distinct(collector)) as collectors
+        WHERE collectors > {threshold}
+        WITH author
+        MATCH (wic:_Wic:_Context:_{context}:_{self.subgraph_name})
+        MERGE (author)-[con:_HAS_CONTEXT]->(wic)
+        RETURN COUNT(DISTINCT(author))"""
+        count = self.query(mirrorConnect)[0].value()
+
+        return count
+
+    @count_query_logging
+    def get_substack_influencer(self, context):
+        substackQuery = f"""
+        MATCH (wallet:Wallet)-[:HAS_ACCOUNT]-(twitter:Twitter:Account)
+        MATCH (wallet)-[:HAS_ACCOUNT]-(substack:Substack:Account)
+        MATCH (wic:_Wic:_Context:_{self.subgraph_name}:_{context})
+        WITH wallet, wic
+        MERGE (wallet)-[con:_HAS_CONTEXT]->(wic)
+        RETURN COUNT(DISTINCT(wallet))
+        """
+        count = self.query(substackQuery)[0].value()
+
+        return count 
+
+    
+    
+
     @count_query_logging
     def identify_podcasters_bios(self, context, queryString):
- 
         label = f"""
         CALL db.index.fulltext.queryNodes("wicBios", "{queryString}") 
         YIELD node
